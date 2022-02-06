67c46e8181081fd350eebe374b4669a43d70008a
lomasiltre23 <charlsmuvill@gmail.com>
2022-02-06 16:13:10 -0600

    YK-13 Create repo with unreal files

---
+++ Source/Yumkimil1/Yumkimil1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Yumkimil1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YUMKIMIL1_API AYumkimil1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
